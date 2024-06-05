17b3636e3d0ac78425c52d1e98e4442db48ad054
dalex <dalex5942@gmail.com>
2024-06-05 15:37:54 +1000

    Added SSharedLib type and functions (no testing).

--- build/source/Engine/FileIO.h
+++ build/source/Engine/FileIO.h
@@ -1,6 +1,12 @@
 #ifndef _ENGINE_FILESYSTEM_H_
 #define _ENGINE_FILESYSTEM_H_
 
+#include "Stl/String.h"
+
+#ifdef PLATFORM_WIN64
+#	undef LoadLibrary
+#endif
+
 struct SFileBlob
 {
 	size_t Size;
@@ -26,5 +32,19 @@ struct SMappedFile
 };
 bool MapFile(const TCHAR *Path, SMappedFile *pOut);
 void UnmapFile(SMappedFile *pFile);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Named after the Win32 name for a DLL because SDLL is too small and stinky.
+struct SSharedLib
+{
+	void *Handle;
+	size_t LastWrite;
+	TString<TCHAR> BaseFileName; // Name of the DLL that has been copied.
+};
+bool LoadSharedLib(SSharedLib *pLib, TString<TCHAR> Path, bool bCopyFile);
+void ReleaseSharedLib(SSharedLib *pLib);
+bool IsSharedLibDirty(SSharedLib *pLib);
+void *LoadProc(SSharedLib pLib, const char *ProcName);
 
 #endif // _ENGINE_FILESYSTEM_H_
--- build/source/Engine/FileIOWin64.cpp
+++ build/source/Engine/FileIOWin64.cpp
@@ -25,12 +25,12 @@ bool ReadEntireFile(const TCHAR *Path, SFileBlob *pBlob)
 	return false;
 }
 
-bool MapFile(const TCHAR *Path, SMappedFile *pOut)
+bool MapFile(const TCHAR *Path, SMappedFile *pFile)
 {
 	pFile->Handle = (void *)::CreateFile(Path, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 128, nullptr);
 	if (pFile->Handle != (void *)(HANDLE)-1) {
 		LARGE_INTEGER Li;
-		::GetFileSize((HANDLE)pFile->Handle, &Li);
+		::GetFileSizeEx((HANDLE)(pFile->Handle), &Li);
 		pFile->Size = Li.QuadPart;
 
 		HANDLE mapping = ::CreateFileMapping((HANDLE)pFile->Handle, nullptr, PAGE_READWRITE, 0, 0, nullptr);
@@ -50,4 +50,62 @@ void UnmapFile(SMappedFile *pFile)
 		::CloseHandle((HANDLE)pFile->Handle);
 		ZeroThat(pFile);
 	}
+}
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+bool LoadSharedLib(SSharedLib *pLib, TString<TCHAR> Path, bool bCopyFile)
+{
+	ZeroThat(pLib);
+
+	if (!bCopyFile) {
+		pLib->BaseFileName = TString<TCHAR>();
+		pLib->Handle = ::LoadLibrary(Path);
+		if (pLib->Handle)
+			return true;
+		return false;
+	}
+
+	pLib->BaseFileName = Path;
+
+	Path.Insert(STR("_LOCKED"), Path.GetFileExtension());
+	CopyFile(pLib->BaseFileName, Path, false);
+
+	pLib->Handle = ::LoadLibrary(Path);
+	
+	WIN32_FILE_ATTRIBUTE_DATA Fad = {};
+	::GetFileAttributesEx((TCHAR *)pLib->BaseFileName, GetFileExInfoStandard, &Fad);
+
+	pLib->LastWrite = (size_t)Fad.ftLastWriteTime.dwHighDateTime << 32 | Fad.ftLastWriteTime.dwLowDateTime;
+
+	return true;
+}
+
+void ReleaseSharedLib(SSharedLib *pLib)
+{
+	::FreeLibrary((HMODULE)pLib->Handle);
+	if (pLib->LastWrite != 0) {
+		pLib->BaseFileName.Insert(STR("_LOCKED"), pLib->BaseFileName.GetFileExtension());
+		::DeleteFile((TCHAR *)pLib->BaseFileName);
+	}
+	ZeroThat(pLib);
+	pLib->BaseFileName = TString<TCHAR>();
+}
+
+bool IsSharedLibDirty(SSharedLib *pLib)
+{
+	if (pLib->LastWrite != 0) {
+		WIN32_FILE_ATTRIBUTE_DATA Fad = {};
+		::GetFileAttributesEx(pLib->BaseFileName, GetFileExInfoStandard, &Fad);
+
+		if (((size_t)Fad.ftLastWriteTime.dwHighDateTime << 32 | Fad.ftLastWriteTime.dwLowDateTime) > pLib->LastWrite) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void *LoadProc(SSharedLib pLib, const char *ProcName)
+{
+	return ::GetProcAddress((HMODULE)pLib.Handle, ProcName);
 }
